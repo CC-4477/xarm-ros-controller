35ba3d33ec0a040638b694e8d6814026637a2405
sameeri2 <sameeri2@illinois.edu>
2023-06-16 18:31:47 +0000

    ditto

--- scripts/xarm_home_controller.py
+++ scripts/xarm_home_controller.py
@@ -6,7 +6,7 @@ from xarm_msgs.srv import Move, MoveRequest, SetInt16, SetInt16Request
 from std_msgs.msg import Int16
 
 def move_callback(home_msg):
-    tar_home = Move()
+    tar_home = MoveRequest()
     tar_home.pose = home_msg.pose
     tar_home.mvvelo = home_msg.mvvelo
     tar_home.mvacc = home_msg.mvacc
@@ -15,18 +15,18 @@ def move_callback(home_msg):
 def state_callback(state_val):
     state_value = SetInt16()
     state_value = state_val.data
-    set_state_service(srv_go)
+    set_state_service(state_value)
     
 if __name__ == '__main__':
     rospy.init_node('go_home_node')
     srv_go = SetInt16()
     srv_go.data = 0
     set_state_service = rospy.ServiceProxy('ufactory/set_state', SetInt16)
-    go_home_service = rospy.ServiceProxy('/ufactory/go_home', Move)
+    go_home_service = rospy.ServiceProxy('/ufactory/go_home', MoveRequest)
 
 
     rospy.Subscriber("set_state", Int16, state_callback)
-    rospy.Subscriber("go_home", Move, move_callback)
+    rospy.Subscriber("go_home", MoveRequest, move_callback)
     rospy.wait_for_service('/ufactory/go_home')
     rospy.wait_for_service('/ufactory/set_state')
 
